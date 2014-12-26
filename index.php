4da9be0d23fb7ecb9957dca7dc9878cf8f65592b
xness <sebastiano@bluedigital.cl>
2014-12-26 18:51:53 -0300

    First Commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>NIVEA Aclarado | Con mis amigas soy natural</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+        
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+		<!-- WRAPPER -->
+        <div id="wrapper">
+        	<header class="top">
+        		<nav class="menu">
+        			<ul>
+        				<li><a href="#">Galer&iacute;a</a></li>
+        				<li><a href="#">Ver Bases</a></li>
+        				<li><a href="#"><img src="img/icon-compartir.png" alt="Compartir" /> Compartir</a></li>
+        			</ul>
+        		</nav>
+        	</header>
+        	<div class="content">
+        		<div class="logo">
+        			<img src="img/logo.png" alt="Con mis amigas soy natural" />
+        		</div>
+        		<div class="titulo">
+        			<h1>Nivea aclarado natural te da la posibilidad<br /> 
+						de disfrutar naturalmente junto a tus amigas</h1>
+					<img src="img/titulo_line.png" alt="" />
+        		</div>
+        		<div class="bajada">
+        			<h2>Arma tu caba&ntilde;a, inv&iacute;ta a tres amigas y participa por un fin de semana so&ntilde;ado. La caba&ntilde;a con m&aacute;s votos ser&aacute; la ganadora.</h2>
+        		</div>
+        		<div class="participar">
+        			<a href="#" class="btn"><img src="img/btn_participar.png" alt="Participar" /></a>
+        		</div>
+        	</div>
+        </div> <!-- #WRAPPER -->
+        
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
